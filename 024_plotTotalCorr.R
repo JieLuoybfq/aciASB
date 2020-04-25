e7cf2ca5fe2e69f5fd43e5e08d4274588c774d4a
goergen95 <darius2402@web.de>
2020-04-25 16:39:47 +0200

    word count

--- 024_plotTotalCorr.R
+++ 024_plotTotalCorr.R
@@ -10,5 +10,6 @@ plt = ggplot(data=totalCorr, aes(y=estimate, x=contVar, fill=contVar)) +
   xlab("Control Variables")+
   labs(fill = "Control Variables")+
   theme_classic()+
-  coord_flip()
+  coord_flip()+
+  theme(text = element_text(size=15))
 ggsave(filename="../results/plots/totalCorr.png", plt)
